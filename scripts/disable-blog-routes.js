2fd31b2529440ac5b04a56f70e5c2da4027f7002
hello8693 <hello8693@hello8693.xyz>
2026-02-25 11:14:04 +0800

    feat: 更新导航菜单，启用友链功能并调整路由过滤

--- scripts/disable-blog-routes.js
+++ scripts/disable-blog-routes.js
@@ -1,5 +1,5 @@
 hexo.extend.filter.register('after_generate', () => {
-  const removePrefixes = ['tags/', 'categories/', 'archives/', 'blog/', '_moments/'];
+  const removePrefixes = ['_moments/'];
 
   hexo.route.list().forEach((route) => {
     if (removePrefixes.some((prefix) => route.startsWith(prefix))) {
